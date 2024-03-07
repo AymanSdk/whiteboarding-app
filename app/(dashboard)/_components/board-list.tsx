412cdc132c7c144bd0f16cf64c1dae1bee4a0664
AymanSdk <ayman.sdk123@oulook.com>
2024-03-07 21:33:36 +0100

    Add NewBoardButton component to BoardList

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -5,6 +5,7 @@ import { EmptyBoards } from './empty-boards';
 import { EmptyFavorites } from './empty-favorites';
 import { EmptySearch } from './empty-search';
 import { BoardCard } from './board-card';
+import { NewBoardButton } from './new-board-button';
 
 interface BoardListProps {
 	orgId: string;
@@ -18,7 +19,20 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
 	const data = useQuery(api.boards.get, { orgId }); // ! this my lead to rendering the {"favorites":"true"}
 
 	if (data === undefined) {
-		return <div>Loading...</div>;
+		return (
+			<div>
+				<h2 className='text-3xl'>
+					{query.favorites ? 'Favorite boards' : 'Team boards'}
+				</h2>
+				<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10'>
+					<NewBoardButton orgId={orgId} disabled />
+					<BoardCard.Skeleton />
+					<BoardCard.Skeleton />
+					<BoardCard.Skeleton />
+					<BoardCard.Skeleton />
+				</div>
+			</div>
+		);
 	}
 
 	if (!data?.length && query.search) {
@@ -34,9 +48,12 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
 	}
 
 	return (
-		<div className='text-3xl'>
-			<h2>{query.favorites ? 'Favorite boards' : 'Team boards'}</h2>
+		<div>
+			<h2 className='text-3xl'>
+				{query.favorites ? 'Favorite boards' : 'Team boards'}
+			</h2>
 			<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10'>
+				<NewBoardButton orgId={orgId} />
 				{data?.map((board) => (
 					<BoardCard
 						key={board._id}
