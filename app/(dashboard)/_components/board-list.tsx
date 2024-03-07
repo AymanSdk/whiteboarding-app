ec53b586a133be7b779f6fa4c82ff54caa09e679
AymanSdk <ayman.sdk123@oulook.com>
2024-03-07 15:21:07 +0100

    Add BoardCard component and Overlay component to display board cards in BoardList

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -4,6 +4,7 @@ import { api } from '@/convex/_generated/api';
 import { EmptyBoards } from './empty-boards';
 import { EmptyFavorites } from './empty-favorites';
 import { EmptySearch } from './empty-search';
+import { BoardCard } from './board-card';
 
 interface BoardListProps {
 	orgId: string;
@@ -36,7 +37,19 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
 		<div className='text-3xl'>
 			<h2>{query.favorites ? 'Favorite boards' : 'Team boards'}</h2>
 			<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10'>
-				Board Cards go here!!
+				{data?.map((board) => (
+					<BoardCard
+						key={board._id}
+						id={board._id}
+						title={board.title}
+						imageUrl={board.imageUrl}
+						authorId={board.authorId}
+						authorName={board.authorName}
+						createdAt={board._creationTime}
+						orgId={board.orgId}
+						isFavorite={false}
+					/>
+				))}
 			</div>
 		</div>
 	);
