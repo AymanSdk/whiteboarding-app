1bd18ff00d78de0d699fa8e4f8b56074ad17e72f
AymanSdk <ayman.sdk123@oulook.com>
2024-03-24 21:10:39 +0000

    Add Cursor component import to Cursors component

--- app/board/[boardId]/_components/cursors-presence.tsx
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -3,6 +3,7 @@
 // * module imports *
 import { memo } from 'react';
 import { useOthersConnectionIds } from '@/liveblocks.config';
+import { Cursor } from './cursor';
 
 const Cursors = () => {
 	const ids = useOthersConnectionIds();
