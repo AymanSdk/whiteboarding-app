9ab0725ee1fe6d0bae6cdd31d19c73d242bf4cf3
AymanSdk <ayman.sdk123@oulook.com>
2024-03-24 21:10:21 +0000

    Update canvas component with new features

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,8 +1,13 @@
 'use client';
 // * module imports *
-import { useState } from 'react';
-import { useHistory, useCanRedo, useCanUndo } from '@/liveblocks.config';
-import { CanvasMode, CanvasState } from '@/types/canvas';
+import React, { useState, useCallback } from 'react';
+import {
+	useHistory,
+	useCanRedo,
+	useCanUndo,
+	useMutation
+} from '@/liveblocks.config';
+import { Camera, CanvasMode, CanvasState } from '@/types/canvas';
 // * components imports *
 import { Info } from './info';
 import { Participants } from './participants';
@@ -17,10 +22,30 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 	const [canvasState, setCanvasState] = useState<CanvasState>({
 		mode: CanvasMode.None
 	});
+	const [Camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
 
 	const history = useHistory();
 	const canUndo = useCanUndo();
 	const canRedo = useCanRedo();
+
+	const onWheel = useCallback((e: React.WheelEvent) => {
+		setCamera((camera) => ({
+			x: camera.x + e.deltaX,
+			y: camera.y + e.deltaY
+		}));
+	}, []);
+
+	// ! display the cursor of the user on the canvas and update it when the user moves the cursor
+	const onPointerMove = useMutation(
+		({ setMyPresence }, e: React.PointerEvent) => {
+			e.preventDefault();
+
+			const current = { x: 0, y: 0 };
+
+			setMyPresence({ cursor: current });
+		},
+		[]
+	);
 
 	return (
 		<main className='h-full w-full relative bg-neutral-100 touch-none'>
@@ -34,7 +59,11 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 				undo={history.undo}
 				redo={history.redo}
 			/>
-			<svg className=' h-[100vh] w-[100vw]'>
+			<svg
+				className=' h-[100vh] w-[100vw]'
+				onWheel={onWheel}
+				onPointerMove={onPointerMove}
+			>
 				<g>
 					<CursorsPresence />
 				</g>
